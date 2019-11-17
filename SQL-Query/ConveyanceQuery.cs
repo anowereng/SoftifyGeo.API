adf423a2052b99b61271ee8aed3f18861bb012ea
anowereng <anowereng@gmail.com>
2019-11-17 17:01:24 +0600

    ConveyanceQuery amount updated..

--- SQL-Query/ConveyanceQuery.cs
+++ SQL-Query/ConveyanceQuery.cs
@@ -19,7 +19,7 @@ namespace SoftifyGEO.API.SQL_Query
     public class ConveyanceQuery : IConveyanceQuery
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
-      
+
         public ConveyanceQuery(IHttpContextAccessor httpContextAccessor)
         {
             _httpContextAccessor = httpContextAccessor;
@@ -33,6 +33,27 @@ namespace SoftifyGEO.API.SQL_Query
             dsList = CoreSQL.CoreSQL_GetDataSet(strQuery);
             return clsCommon.JsonSerialize(dsList.Tables[0]);
         }
+
+        public void SaveUpdateConveyance(Conveyance model)
+        {
+            try
+            {
+                var userid = _httpContextAccessor.HttpContext.User.GetLoggedInUserId<string>();
+                if (string.IsNullOrEmpty(userid))
+                    throw new InvalidOperationException("User Not found");
+                CoreSQLConnection CoreSQL = new CoreSQLConnection();
+                var Query = "SELECT  cast(count( Isnull(visitid,0))  AS float)  AS visitid FROM tblConveyance_Main where visitid = '" + model.visitId + "'";
+                var visitid = CoreSQL.CoreSQL_GetDoubleData(Query);
+                if (visitid > 0)
+                    UpdateConveyance(model);
+                else
+                    SaveConveyance(model);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException(ex.Message);
+            }
+        }
         public string SaveConveyance(Conveyance model)
         {
             var userid = _httpContextAccessor.HttpContext.User.GetLoggedInUserId<string>();
@@ -44,18 +65,45 @@ namespace SoftifyGEO.API.SQL_Query
             var Query = "SELECT  cast(Isnull(MAX(ConveyId),0) + 1 AS float)  AS ConveyId FROM tblConveyance_Main";
             var NewId = CoreSQL.CoreSQL_GetDoubleData(Query);
 
-            var sqlQuery = "Insert Into tblConveyance_Main(ConveyId, dtConveyance, LUserId)" +
-                           " Values ('" + NewId + "', getdate(),'" + userid + "')";
+            var sqlQuery = "Insert Into tblConveyance_Main(ConveyId, dtConveyance, LUserId, visitid)" +
+                           " Values ('" + NewId + "', getdate(),'" + userid + "','" + model.visitId + "')";
             arrayList.Add(sqlQuery);
 
             sqlQuery = "Insert Into tblConveyance_Sub(ConveyId, ConveyTypeId, ConveyAmount)" +
                       " Values ('" + NewId + "', '" + model.conveyTypeId + "', '" + model.conveyAmount + "')";
             arrayList.Add(sqlQuery);
 
-       
+
             CoreSQL.CoreSQL_SaveDataUseSQLCommand(arrayList);
             return "Success";
         }
 
+        public string UpdateConveyance(Conveyance model)
+        {
+            var userid = _httpContextAccessor.HttpContext.User.GetLoggedInUserId<string>();
+            if (string.IsNullOrEmpty(userid))
+                throw new InvalidOperationException("User Not found");
+
+            CoreSQLConnection CoreSQL = new CoreSQLConnection();
+            ArrayList arrayList = new ArrayList();
+
+            var Query = "SELECT  cast(Isnull(ConveyId,0)  AS float)  AS ConveyId FROM tblConveyance_Main where visitid = '" + model.visitId + "'";
+            var ConveyId = CoreSQL.CoreSQL_GetDoubleData(Query);
+            if (ConveyId == 0)
+                throw new InvalidOperationException("Conveyance History Not found");
+
+            var sqlQuery = "delete tblConveyance_Sub where ConveyId = '" + ConveyId + "'";
+            arrayList.Add(sqlQuery);
+
+            sqlQuery = "Update tblConveyance_Main set dtConveyance = getdate() , LUserId = '" + userid + "' where ConveyId = '" + ConveyId + "'";
+            arrayList.Add(sqlQuery);
+
+            sqlQuery = "Insert Into tblConveyance_Sub(ConveyId, ConveyTypeId, ConveyAmount)" +
+                      " Values ('" + ConveyId + "', '" + model.conveyTypeId + "', '" + model.conveyAmount + "')";
+            arrayList.Add(sqlQuery); ;
+
+            CoreSQL.CoreSQL_SaveDataUseSQLCommand(arrayList);
+            return "Success";
+        }
     }
 }
