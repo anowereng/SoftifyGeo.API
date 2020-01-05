fe88553d001e5953228325b1f25a519355435004
anowereng <anowereng@gmail.com>
2020-01-05 12:04:42 +0600

    .

---
+++ Interfaces/ILoginQuery.cs
@@ -0,0 +1,15 @@
+﻿using SoftifyGEO.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftifyGEO.API.Interfaces
+{
+    public interface ILoginQuery
+    {
+        bool UserExists(string username);
+        string UpdateProfile(User model);
+        string GetUser();
+    }
+}
